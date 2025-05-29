d1c87b4299c7759af1aff0ba427afb741da8b3bd
jyhong <50126875+ghdwodud@users.noreply.github.com>
2025-05-29 17:20:29 +0900

    로고 이미지 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,19 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="h-10 bg-white border-b">
-  <div className="h-full flex items-center">
-    <div className="pl-[151px] flex justify-between items-center w-full max-w-screen-xl mx-auto">
-      {/* 로고 */}
-      <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="사이트 로고"
-          width={80}
-          height={24}
-        />
-      </Link>
-    </div>
-  </div>
-</header>
-
+      <div className="h-full flex items-center">
+        <div className="pl-[151px] flex justify-between items-center w-full max-w-screen-xl mx-auto">
+          {/* 로고 */}
+          <Link href="/">
+            <Image
+              src="/assets/img-1.png"
+              alt="사이트 로고"
+              width={80}
+              height={24}
+            />
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 }
