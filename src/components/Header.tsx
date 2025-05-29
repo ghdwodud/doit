5a3aa24d77728a5b47a88e4b21086c13ee8739d8
jyhong <50126875+ghdwodud@users.noreply.github.com>
2025-05-29 18:11:05 +0900

    모바일 태블릿 화면 로고 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,25 @@ export default function Header() {
   return (
     <header className="h-10 bg-white border-b">
       <div className="h-full flex items-center">
-        <div className="pl-[151px] flex justify-between items-center w-full max-w-screen-xl mx-auto">
+        <div className="pl-4 sm:pl-[151px] flex justify-between items-center w-full max-w-screen-xl mx-auto">
           {/* 로고 */}
           <Link href="/">
+            {/* 데스크탑용 로고 */}
             <Image
-              src="/assets/img-1.png"
+              src="/assets/Size=Large.png"
               alt="사이트 로고"
               width={80}
               height={24}
+              className="hidden sm:block"
+            />
+
+            {/* 모바일/태블릿용 로고 */}
+            <Image
+              src="/assets/Size=Small.png"
+              alt="모바일 로고"
+              width={40}
+              height={24}
+              className="block sm:hidden"
             />
           </Link>
         </div>
